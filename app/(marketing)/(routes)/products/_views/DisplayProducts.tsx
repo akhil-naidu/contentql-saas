14d6510fa1466412afb9ae73d36a047abdd98353
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2023-12-04 18:02:52 +0530

    chore: ui done

--- app/(marketing)/(routes)/products/_views/DisplayProducts.tsx
+++ app/(marketing)/(routes)/products/_views/DisplayProducts.tsx
@@ -1,17 +1,41 @@
+'use client';
+
 import React from 'react';
 import ProductCard from '../_components/ProductCard';
+import { useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
 
 const DisplayProducts = () => {
-  const products = [
-    { id: 1, name: 'shoes', description: 'my favorite shoes' },
-    { id: 2, name: 'shoes', description: 'my favorite shoes' },
-  ];
+  // const products = [
+  //   { id: 1, name: 'shoes', description: 'my favorite shoes' },
+  //   { id: 2, name: 'shoes', description: 'my favorite shoes' },
+  // ];
+
+  const products = useQuery(api.products.get);
 
   return (
-    <div className='flex gap-4'>
-      {products.map(({ id, name, description }) => {
-        return <ProductCard key={id} name={name} description={description} />;
-      })}
+    <div className='flex flex-col gap-4 mx-auto'>
+      <div className='flex gap-2 flex-col'>
+        <input
+          type='text'
+          placeholder='Enter Product Name'
+          className='input w-full max-w-xs'
+        />
+
+        <input
+          type='text'
+          placeholder='Enter Product Description'
+          className='input w-full max-w-xs'
+        />
+      </div>
+
+      <div className='flex gap-4'>
+        {products?.map(({ _id, name, description }) => {
+          return (
+            <ProductCard key={_id} name={name} description={description} />
+          );
+        })}
+      </div>
     </div>
   );
 };
--- convex/products.ts
+++ convex/products.ts
@@ -1,8 +1,20 @@
-import { query } from './_generated/server';
+import { argv } from 'process';
+import { query, mutation } from './_generated/server';
+import { v } from 'convex/values';
 
 export const get = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query('products').collect();
+  },
+});
+
+export const add = mutation({
+  args: { name: v.string(), description: v.string() },
+  handler: async (ctx, args) => {
+    return await ctx.db.insert('products', {
+      description: args.description,
+      name: args.name,
+    });
   },
 });
